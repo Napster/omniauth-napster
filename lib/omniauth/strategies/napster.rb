07a2fcac839e46acd3a285b526934dcc67017143
Mark James <mrj@advancedcontrols.com.au>
2017-03-17 13:40:13 +1100

    Switch info from /me to /me/account, and add extra info fields. Update README.

--- lib/omniauth/strategies/napster.rb
+++ lib/omniauth/strategies/napster.rb
@@ -11,14 +11,24 @@ module OmniAuth
         :token_url     => '/oauth/access_token'
       }
 
-      uid{ raw_info['me']['id'] }
+      uid{ raw_info['account']['id'] }
 
       info do
+        account_info = raw_info['account']
+        subscription_info = account_info['subscription']
+        
         {
-          :uid => raw_info['me']['id'],
-          :name => raw_info['me']['realName'],
-          :username => raw_info['me']['screenName'],
-          :image => raw_info['me']['avatar']
+          uid: account_info['id'],
+          first_name: account_info['firstName'],
+          last_name: account_info['lastName'],
+          username: account_info['userName'],
+          screen_name: account_info['screenName'],
+          email: account_info['email'],
+          country_code: account_info['country'],
+          preferred_language: account_info['preferredLanguage'],
+          parental_control_enabled: account_info['parentalControlEnabled'],
+          subscription_state: subscription_info['state'].downcase.to_sym,
+          subscription_suspended: subscription_info['isSuspended']
         }
       end
 
@@ -29,7 +39,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/me').parsed
+        @raw_info ||= access_token.get('/v2.0/me/account').parsed
       end
     end
   end
